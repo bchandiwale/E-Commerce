fd1f0dc9460a5e9cde62293216a242eb50d64002
bchandiwale <86349812+bchandiwale@users.noreply.github.com>
2023-09-24 21:46:08 +0530

    Create productlist.js

---
+++ productlist.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+const ProductList = () => {
+  const { products, addToCart } = useContext(AppContext);
+
+  return (
+    <>
+      <div className="hero is-primary">
+        <div className="hero-body container">
+          <h4 className="title">Our Products</h4>
+        </div>
+      </div>
+      <br />
+      <div className="container">
+        <div className="column columns is-multiline">
+          {products && products.length ? (
+            products.map((product, index) => (
+              <ProductItem
+                product={product}
+                key={index}
+                addToCart={addToCart}
+              />
+            ))
+          ) : (
+            <div className="column">
+              <span className="title has-text-grey-light">
+                No products found!
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
